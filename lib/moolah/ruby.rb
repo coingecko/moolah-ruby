690caf22035d1057835e7a00eceb2a188b90e0f6
TM Lee <tm89lee@gmail.com>
2014-08-04 21:04:29 +0800

    Initial commit - bundle gem creation

---
+++ lib/moolah/ruby.rb
@@ -0,0 +1,7 @@
+require "moolah/ruby/version"
+
+module Moolah
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/moolah/ruby/version.rb
@@ -0,0 +1,5 @@
+module Moolah
+  module Ruby
+    VERSION = "0.0.1"
+  end
+end
