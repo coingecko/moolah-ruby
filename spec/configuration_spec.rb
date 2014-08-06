ac673fff1c0bfe02e1f0311043ee43bc1541e32f
Jonathan Lin <ernsheong@gmail.com>
2014-08-06 23:15:15 +0800

    Specs for api_key and endpoint setting via block

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -2,12 +2,30 @@ require 'moolah'
 
 describe Moolah do
   describe ".configure" do
-    it "allows api_key to be configured" do
+    it "has default api_key value of nil" do
       expect(Moolah.send(:api_key)).to eq(nil)
     end
 
-    it "allows endpoint to be configured" do
+    it "had certain endpoint URL" do
       expect(Moolah.send(:endpoint)).to eq("https://api.moolah.io/v2")
+    end
+
+    it "allows api_key configuration via a block" do
+      sample_api_key = "12345678"
+      Moolah.configure do |config|
+        config.api_key = sample_api_key
+      end
+
+      expect(Moolah.api_key).to eq(sample_api_key)
+    end
+
+    it "allows endpoint configuration via a block" do
+      sample_endpoint = "http://example.com"
+      Moolah.configure do |config|
+        config.endpoint = sample_endpoint
+      end
+
+      expect(Moolah.endpoint).to eq(sample_endpoint)
     end
   end
 end
