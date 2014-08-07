46b591dce3061c4bae8ff53ed1daa785e75151ce
Jonathan Lin <ernsheong@gmail.com>
2014-08-07 23:33:51 +0800

    Stub sanity check

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,23 +1,27 @@
 require 'moolah'
 
 describe Moolah::Client do
-  it "complains when API key is not configured" do
-    expect { Moolah::Client.new }.to raise_error(ArgumentError)
-  end
 
-  context "with API key" do
-    before do
-      allow(Moolah).to receive(:api_key).and_return("1234567890")
+  describe ".initialize" do
+    it "complains when API key is not configured" do
+      expect { Moolah::Client.new }.to raise_error(ArgumentError)
     end
 
-    it "should not complain if API key is given" do
-      expect { Moolah::Client.new }.not_to raise_error
-    end
+    context "with API key" do
+      before do
+        allow(Moolah).to receive(:api_key).and_return("1234567890")
+      end
 
-    it "can take api_secret and ipn as optional parameters" do
-      client = Moolah::Client.new({ ipn: "http://www.example.com", api_secret: "a_secret_key" })
-      expect(client.ipn).to eq("http://www.example.com")
-      expect(client.api_secret).to eq("a_secret_key")
-    end 
+      it "should not complain if API key is given" do
+        expect(Moolah::api_key).to eq("1234567890")
+        expect { Moolah::Client.new }.not_to raise_error
+      end
+
+      it "can take api_secret and ipn as optional parameters" do
+        client = Moolah::Client.new({ ipn: "http://www.example.com", api_secret: "a_secret_key" })
+        expect(client.ipn).to eq("http://www.example.com")
+        expect(client.api_secret).to eq("a_secret_key")
+      end
+    end
   end
 end
