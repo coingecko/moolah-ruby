a649ceb7cc5bb266f28cea69c21d46131efb6d95
Jonathan Lin <ernsheong@gmail.com>
2014-08-09 19:01:28 +0800

    More spec refactoring

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -50,59 +50,58 @@ describe Moolah::Client do
       allow(Moolah).to receive(:api_key).and_return("1234567890")
     end
 
-    context "without optional parameters (ipn, api_secret, ipn_extra)" do
-      let(:client) { Moolah::Client.new }
-      let(:post_path) { "#{action_path}?amount=20&apiKey=1234567890&coin=dogecoin&currency=USD&product=Coingecko+Pro" }
-      let(:json_response) { '{"status":"success","guid":"a4dc89fcc-8ad-3f4c1bf529-6396c1acc4-","url":"https:\/\/pay.moolah.io\/a4dc89fcc-8ad-3f4c1bf529-6396c1acc4-","coin":"dogecoin","amount":"121526.39285714","address":"DS6frMZR5jFVEf9V6pBi9qtcVJa2JX5ewR","timestamp":1407579569}' }
-      before do
-        allow(client).to receive(:connection).and_return(test_connection)
-        request_stubs.post(post_path) { |env| [ 200, {}, json_response ] }
-      end
-
-      shared_examples "successful transaction" do
-        it { expect(transaction.response.status).to eq("success") }
-        it { expect(transaction.response.amount).to eq("121526.39285714") }
-        it { expect(transaction.response.coin).to eq("dogecoin") }
-        it { expect(transaction.response.guid).to eq("a4dc89fcc-8ad-3f4c1bf529-6396c1acc4-") }
-        it { expect(transaction.response.address).to eq("DS6frMZR5jFVEf9V6pBi9qtcVJa2JX5ewR") }
-        it { expect(transaction.response.timestamp).to eq(1407579569) }
-        it { expect(transaction.response.url).to eq("https://pay.moolah.io/a4dc89fcc-8ad-3f4c1bf529-6396c1acc4-") }
-      end
-
-      context "allows transaction params to be given as argument" do
-        let(:transaction) { client.create_transaction transaction_params }
-
-        it_behaves_like "successful transaction"
-      end
-
-      context "allows transaction params to be given in the block" do
-        let(:transaction) do 
-          client.create_transaction do |t|
-            t.coin = "dogecoin"
-            t.currency = "USD"
-            t.amount = "20"
-            t.product = "Coingecko Pro"
-          end
-        end
-
-        it_behaves_like "successful transaction"
-      end
+    shared_examples "a successful transaction" do
+      it { expect(transaction.response).to be_an_instance_of(Moolah::TransactionResponse) }
+      it { expect(transaction.response.status).to eq("success") }
+      it { expect(transaction.response.amount).to eq("121526.39285714") }
+      it { expect(transaction.response.coin).to eq("dogecoin") }
+      it { expect(transaction.response.guid).to eq("a4dc89fcc-8ad-3f4c1bf529-6396c1acc4-") }
+      it { expect(transaction.response.address).to eq("DS6frMZR5jFVEf9V6pBi9qtcVJa2JX5ewR") }
+      it { expect(transaction.response.timestamp).to eq(1407579569) }
+      it { expect(transaction.response.url).to eq("https://pay.moolah.io/a4dc89fcc-8ad-3f4c1bf529-6396c1acc4-") }
     end
 
-    context "with optional parameters" do
-      let(:client) { Moolah::Client.new({ api_secret: "secret", ipn: "www.example.com/processed_payment" }) }
-      let(:post_path) { "#{action_path}?amount=20&apiKey=1234567890&apiSecret=secret&coin=dogecoin&currency=USD&ipn=www.example.com%2Fprocessed_payment&product=Coingecko+Pro" }
-      let(:json_response) { '{"status":"success","guid":"a4dc89fcc-8ad-3f4c1bf529-6396c1acc4-","url":"https:\/\/pay.moolah.io\/a4dc89fcc-8ad-3f4c1bf529-6396c1acc4-","coin":"dogecoin","amount":"121526.39285714","address":"DS6frMZR5jFVEf9V6pBi9qtcVJa2JX5ewR","timestamp":1407579569}' }
-      before do
-        allow(client).to receive(:connection).and_return(test_connection)
-        request_stubs.post(post_path) { |env| [ 200, {}, json_response ] }
+    context "successful transaction" do
+      context "without optional parameters (ipn, api_secret, ipn_extra)" do
+        let(:client) { Moolah::Client.new }
+        let(:post_path) { "#{action_path}?amount=20&apiKey=1234567890&coin=dogecoin&currency=USD&product=Coingecko+Pro" }
+        let(:json_response) { '{"status":"success","guid":"a4dc89fcc-8ad-3f4c1bf529-6396c1acc4-","url":"https:\/\/pay.moolah.io\/a4dc89fcc-8ad-3f4c1bf529-6396c1acc4-","coin":"dogecoin","amount":"121526.39285714","address":"DS6frMZR5jFVEf9V6pBi9qtcVJa2JX5ewR","timestamp":1407579569}' }
+        before do
+          allow(client).to receive(:connection).and_return(test_connection)
+          request_stubs.post(post_path) { |env| [ 200, {}, json_response ] }
+        end
+
+        context "allows transaction params to be given as argument" do
+          let(:transaction) { client.create_transaction transaction_params }
+
+          it_behaves_like "a successful transaction"
+        end
+
+        context "allows transaction params to be given in the block" do
+          let(:transaction) do 
+            client.create_transaction do |t|
+              t.coin = "dogecoin"
+              t.currency = "USD"
+              t.amount = "20"
+              t.product = "Coingecko Pro"
+            end
+          end
+
+          it_behaves_like "a successful transaction"
+        end
       end
 
-      it "sends out request with these optional parameters" do
-        transaction = client.create_transaction transaction_params
+      context "with optional parameters" do
+        before do
+          allow(client).to receive(:connection).and_return(test_connection)
+          request_stubs.post(post_path) { |env| [ 200, {}, json_response ] }
+        end
+        let(:client) { Moolah::Client.new({ api_secret: "secret", ipn: "www.example.com/processed_payment" }) }
+        let(:post_path) { "#{action_path}?amount=20&apiKey=1234567890&apiSecret=secret&coin=dogecoin&currency=USD&ipn=www.example.com%2Fprocessed_payment&product=Coingecko+Pro" }
+        let(:json_response) { '{"status":"success","guid":"a4dc89fcc-8ad-3f4c1bf529-6396c1acc4-","url":"https:\/\/pay.moolah.io\/a4dc89fcc-8ad-3f4c1bf529-6396c1acc4-","coin":"dogecoin","amount":"121526.39285714","address":"DS6frMZR5jFVEf9V6pBi9qtcVJa2JX5ewR","timestamp":1407579569}' }
+        let(:transaction) { client.create_transaction transaction_params }
 
-        expect(transaction.response).to be_an_instance_of(Moolah::TransactionResponse)
-        expect(transaction.response.status).to eq("success")
+        it_behaves_like "a successful transaction"
       end
     end
   end
